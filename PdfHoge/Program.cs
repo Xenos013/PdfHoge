29819030f1abe06aba621535de952f34df254e0c
csigesnpb <csigesnpb@gmail.com>
2022-12-03 14:26:26 +0900

    first commit

---
+++ PdfHoge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using PdfiumViewer;
+
+string path = "sample.pdf";
+const int dpi = 96;
+const int ppi = 72;
+using (PdfDocument doc = PdfDocument.Load(path))
+{
+    for (var pageNum = 0; pageNum < doc.PageCount; pageNum++)
+    {
+        SizeF pageSize = doc.PageSizes[pageNum];
+        int width = (int) (pageSize.Width * dpi / ppi);
+        int height = (int) (pageSize.Height * dpi / ppi);
+        Image image = doc.Render(pageNum, width, height, dpi, dpi, false);
+        image.Save($"image{pageNum + 1}.jpg");
+    }
+}
---
+++ PdfHogeWpf/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using PdfiumViewer;
+
+namespace PdfHogeWpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        string path = "sample.pdf";
+        const int dpi = 96 * 2;
+        const int ppi = 72;
+        int pageNum = 0;
+        BitmapSource bitmapSource;
+        using (PdfDocument doc = PdfDocument.Load(path))
+        {
+            SizeF pageSize = doc.PageSizes[pageNum];
+            int width = (int) (pageSize.Width * dpi / ppi);
+            int height = (int) (pageSize.Height * dpi / ppi);
+            Bitmap bitmap = (Bitmap) doc.Render(pageNum, width, height, dpi, dpi, false);
+            nint hBitmap = bitmap.GetHbitmap();
+            bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap, 
+                nint.Zero, 
+                Int32Rect.Empty, 
+                BitmapSizeOptions.FromEmptyOptions());
+        }
+        image.Source = bitmapSource;
+        this.Height = bitmapSource.Height;
+        this.Width = bitmapSource.Width;
+    }
+}
